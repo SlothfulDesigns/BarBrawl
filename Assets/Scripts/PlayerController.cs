64225fad22b8450e3dcc4e46152c23a59ca29180
anobi <nsalakka@gmail.com>
2015-12-13 21:16:44 +0200

    it moves

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,21 @@ public class PlayerController : MonoBehaviour {
 
 	//player positions
 	public float x, y, z;
+	public float rotation;
+	public float speed;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		float inputH = Input.GetAxis("Horizontal");
+		float inputV = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(inputV, 0.0f, inputH);
+		GetComponent<Rigidbody>().velocity = speed * movement;
 	}
 }
