e3ae92389197c9b638eb274acb5caed78e434426
anobi <nsalakka@gmail.com>
2015-12-13 20:56:28 +0200

    unity project skeleton

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//player positions
+	public float x, y, z;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
