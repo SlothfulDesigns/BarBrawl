c08f817bc194d06850ef800b63f7059cff974a79
Niko Salakka <nsalakka@gmail.com>
2015-12-15 18:48:28 +0200

    controls kinda almost work somehow properlyish now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour {
 
     //player positions
 	public float speed, jumpspeed;
-    private bool falling;
+    private bool grounded;
 
 	private enum Arm
 	{
@@ -16,43 +16,67 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start()
 	{
-        falling = false;
+
 	}
 	
-	// Update is called once per frame
+    //update stuff that shouldn't be synced to fps, like controls, here...
 	void Update()
 	{
         HandleActions();
         HandleMovement();
     }
 
-    void HandleMovement()
+    //...and physics and such here
+    void FixedUpdate()
     {
 
-        if (GetComponent<Rigidbody>().velocity.y < 1) falling = false;
+        grounded = IsGrounded();
+        Debug.Log(IsGrounded());
 
-        //handle rotation first so we can skip handling other movement if the player is currently jumping/falling
+        //disable gravity when on ground so that we don't tumble around
+        //like drunkards. Oh, wait a sec..
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = !grounded;
+        rigidbody.drag = !grounded ? 0 : 10;
+    }
+
+    //check the distance from collider bounds to ground below
+    //to see if we're grounded or in mid flight for some reason
+    bool IsGrounded()
+    {
+        Collider collider = GetComponent<Collider>();
+        float distance = collider.bounds.extents.y;
+
+        return Physics.Raycast(transform.position, -Vector3.up, distance + 0.1f);
+    }
+
+    void HandleMovement()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        //handle rotation first so we can skip handling other movement if the player is currently jumping/grounded
         Vector3 inputRotation = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
         if (inputRotation != Vector3.zero)
-            GetComponent<Rigidbody>().rotation = Quaternion.LookRotation(inputRotation);
+            rigidbody.rotation = Quaternion.LookRotation(inputRotation);
 
-        //skip movement if falling
-        if (falling) return;
+        //skip movement if grounded to disable air steering
+        if (!grounded) return;
 
         //handle moving around
         float inputHorizontal = Input.GetAxisRaw("Horizontal");
         float inputVertical = Input.GetAxisRaw("Vertical");
         Vector3 movement = new Vector3(inputHorizontal, 0.0f, inputVertical);
 
-        GetComponent<Rigidbody>().velocity = speed * movement;
-
+        rigidbody.velocity += movement;
     }
+
     void HandleActions()
     {
-        if (Input.GetButtonDown("Jump") && !falling)
+        //disable when in flight, no doublejumping allowed here
+        if (Input.GetButtonDown("Jump") && grounded)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpspeed);
-            falling = true;
+            GetComponent<Rigidbody>().velocity += Vector3.up * jumpspeed;
+            grounded = false;
             Debug.Log("Jump!");
         }
 
