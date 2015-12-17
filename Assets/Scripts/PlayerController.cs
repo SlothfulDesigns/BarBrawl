6d436b37391745db32a6773c6c020f0a3f541c6d
Niko Salakka <nsalakka@gmail.com>
2015-12-17 20:50:06 +0200

    crude punching functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     //player positions
-	public float speed, jumpspeed;
+	public float speed, jumpspeed, punchForce;
     private bool grounded;
 
 	private enum Arm
@@ -30,7 +30,6 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate()
     {
         grounded = IsGrounded();
-        Debug.Log(IsGrounded());
 
         //disable gravity when on ground so that we don't tumble around
         //like drunkards. Oh, wait a sec..
@@ -46,7 +45,7 @@ public class PlayerController : MonoBehaviour {
         Collider collider = GetComponent<Collider>();
         float distance = collider.bounds.extents.y;
 
-        return Physics.Raycast(transform.position, -Vector3.up, distance + 0.01f);
+        return Physics.Raycast(transform.position, -Vector3.up, distance + 0.0f);
     }
 
     void HandleMovement()
@@ -95,11 +94,40 @@ public class PlayerController : MonoBehaviour {
 	void Punch(Arm arm)
 	{
 
-        Debug.Log(string.Format("{0} arm punch!", arm));
 		//get the fist subcomponent of the selected arm
 		//get the direction the player is facing
+        var direction = transform.forward;
+
 		//get the closest object in that direction
+        Rigidbody target = GetNearestTarget();
+
+        if(target != null)
+        {
 		//add force to fist, towards the targeted object
+        target.velocity += direction.normalized * punchForce;
+
 		//profit!
+        Debug.Log(string.Format("{0} arm punch!", arm));
+        }
 	}
+
+    private Rigidbody GetNearestTarget()
+    {
+        Rigidbody target = null;
+        var bumper = transform.Find("radarlol").GetComponent<SphereCollider>();
+        Collider[] collider = Physics.OverlapSphere(bumper.transform.position, bumper.radius);
+        foreach(var collision in collider)
+        {
+            //ignore self
+            if (collision.name == this.name) continue;
+
+            //ignore non-gravitational objects, ie. map and static stuff
+            var rigidbody = collision.attachedRigidbody;
+            if(rigidbody != null && rigidbody.useGravity)
+            {
+                target = collision.attachedRigidbody;
+            }
+        }
+        return target;
+    }
 }
